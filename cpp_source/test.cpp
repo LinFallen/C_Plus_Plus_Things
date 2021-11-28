6cc986dfbde68f48f9abc3414902fe0318eacc27
LinFallen <1664691058@qq.com>
2021-11-28 20:19:51 +0800

    Test Environment

--- cpp_source/test.cpp
+++ cpp_source/test.cpp
@@ -6,18 +6,6 @@ int main() {
 
     cout << "Hello World!" << endl;
 
-    // judge a number is odd or even
-    int num = 0;
-    cout << "Please input a number: ";
-    cin >> num;
-    if (num % 2 == 0) {
-        cout << "The number is even." << endl;
-    } else {
-        cout << "The number is odd." << endl;
-    }
-    
-
-
     cin.get();
 
     return 0;
